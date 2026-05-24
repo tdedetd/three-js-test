2271d5215216a40107e28a9f48315b1d2ed9de4e
tdedetd <tdedetd@gmail.com>
2026-05-24 20:53:25 +0300

    generateCubeWithNoise

--- src/app/components/scene/utils/functions/generate-cubes-grid.ts
+++ src/app/components/scene/utils/functions/generate-cubes-grid.ts
@@ -15,18 +15,28 @@ export function generateCubesGrid(
     gridSize: 5,
   }, 574829103718473);
 
-  const result = coordsInterval2d({ min: 0.5, max: length - 1 + 0.5 }, (x, y) => {
-    const cubeHeight = perlinNoise.getValue(x, y);
-    const color = mixColors([0, 0, 0], [255, 255, 255], (cubeHeight - minHeight) / (maxHeight - minHeight));
-    const cube = generateCube(new THREE.Color(`rgb(${color[0]}, ${color[1]}, ${color[2]})`));
-
-    cube.position.x = x;
-    cube.position.z = y;
-    cube.position.y = cubeHeight;
-    scene.add(cube);
-
-    return cubeHeight;
-  });
+  const result = coordsInterval2d(
+    { min: 0.5, max: length - 1 + 0.5 },
+    (x, y) => generateCubeWithNoise(x, y, scene, perlinNoise),
+  );
 
   console.info(result);
+}
+
+function generateCubeWithNoise(
+  x: number,
+  y: number,
+  scene: THREE.Scene<THREE.Object3DEventMap>,
+  perlinNoise: PerlinNoise,
+): number {
+  const cubeHeight = perlinNoise.getValue(x, y);
+  const color = mixColors([0, 0, 0], [255, 255, 255], (cubeHeight - minHeight) / (maxHeight - minHeight));
+  const cube = generateCube(new THREE.Color(`rgb(${color[0]}, ${color[1]}, ${color[2]})`));
+
+  cube.position.x = x;
+  cube.position.z = y;
+  cube.position.y = cubeHeight;
+  scene.add(cube);
+
+  return cubeHeight;
 }
