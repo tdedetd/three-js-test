dd78278ac70fc61cee37b0a082257175ebbf80d8
tdedetd <tdedetd@gmail.com>
2026-05-28 17:07:24 +0300

    coordsInterval2d count

--- src/app/utils/functions/coords-interval-2d.ts
+++ src/app/utils/functions/coords-interval-2d.ts
@@ -7,20 +7,20 @@ interface CoordsIntervalOptions {
 export function coordsInterval2d(
   { min, max, interval }: CoordsIntervalOptions,
   fn: (x: number, y: number) => number,
-): { min: number, max: number } {
+): { min: number, max: number, count: number } {
   const intervalResult = interval ?? 1;
   let result = {
     min: Number.MAX_SAFE_INTEGER,
     max: -Number.MAX_SAFE_INTEGER,
+    count: 0,
   };
 
   for (let y = min; y <= max; y += intervalResult) {
     for (let x = min; x <= max; x += intervalResult) {
       const value = fn(x, y);
-      result = {
-        min: Math.min(result.min, value),
-        max: Math.max(result.max, value),
-      };
+      result.min = Math.min(result.min, value);
+      result.max = Math.max(result.max, value);
+      result.count += 1;
     }
   }
 
